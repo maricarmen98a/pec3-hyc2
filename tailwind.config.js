cdbe1f8cf48c1db419db476796d8860b1b0eeaaf
Carmen Arias <maricarmen98a@gmail.com>
2022-05-17 21:32:43 +0200

    terminado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,11 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     screens: {
-
       'tablet': {'min': '768px', 'max': '1199px'},
       'laptop': '1200px'
     },
-    extend: {
-      backgroundImage: {
-        'grid-bg': "url('/src/assets/images/bg1.webp')",
-      }
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
     },
   },
   plugins: [],
